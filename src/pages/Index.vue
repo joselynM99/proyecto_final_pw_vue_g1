45e53fb6dd0c7e8361f7a3c7d170e871f6559dc1
dennisyaj <dennispilco@gmail.com>
2023-03-09 10:41:39 -0500

    portada en index

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,12 +1,19 @@
 <template>
-  <h1>Index</h1>
+
+    <img src="@/img/avis.png" alt="Imagen principal del sistema">
+
 </template>
 
 <script>
 export default {
-  
+
 };
 </script>
 
 <style>
+img {
+  height: 100vh;
+  width: 100%;
+
+}
 </style>
