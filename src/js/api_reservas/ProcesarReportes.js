77b4bebb7916b7a10414a76c04d10519fd74a0ff
dennisyaj <dennispilco@gmail.com>
2023-03-09 10:27:18 -0500

    cambio a ProcesarReportes

--- src/js/api_reservas/ProcesarReportes.js
+++ src/js/api_reservas/ProcesarReportes.js
@@ -19,3 +19,13 @@ const reporteClientesVIP = async () => {
 
     return data
 }
+
+export const listaVehiculosVip=async(inicio)=>{
+    return await obtenerVehiculosVip(inicio);
+}
+
+const obtenerVehiculosVip=async(inicio)=>{
+    const data=axios.get(`http://localhost:8081/API/Reservas/V1/reportes/VehiculoVip?fechaInicio=${inicio}`).then(r => r.data)
+    console.log(data)
+    return data
+}
