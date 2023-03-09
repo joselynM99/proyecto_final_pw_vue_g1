06a2b8659cb0538ab288fea4954f54d2c1f79024
Jorge Sanchez <50539993+Standinrafael@users.noreply.github.com>
2023-03-09 15:47:18 -0500

    Arreglo mensajes

--- src/pages/empleado/BuscarVehiculo.vue
+++ src/pages/empleado/BuscarVehiculo.vue
@@ -2,14 +2,26 @@
   <div class="container mt-5">
     <h1>Buscar vehiculo</h1>
     <div class="form-floating mb-3">
-      <input type="text" name="marca" id="marca" class="form-control" v-model="marca">
+      <input
+        type="text"
+        name="marca"
+        id="marca"
+        class="form-control"
+        v-model="marca"
+      />
       <label for="marca">Marca:</label>
     </div>
     <button class="btn btn-primary" @click="buscarVehiculo">
       <i class="bi bi-search"></i>
       Buscar
     </button>
-    <Mensaje class="my-3" v-if="msg" :tipoAlerta="msg.tipoAlerta" :mensaje="msg.mensaje" :mensajeAdicional="msg.adicional"/>
+    <Mensaje
+      class="my-3"
+      v-if="msg"
+      :tipoAlerta="msg.tipoAlerta"
+      :mensaje="msg.mensaje"
+      :mensajeAdicional="msg.adicional"
+    />
     <div v-if="listaDeVehiculos.length != 0">
       <table class="table mt-5">
         <thead class="table-dark">
@@ -28,13 +40,23 @@
             <td>{{ vehiculo.modelo }}</td>
             <td>{{ vehiculo.placa }}</td>
             <td>
-              <router-link :to="{ name: 'visualizar-vehiculo', params: { id: vehiculo.id } }">
+              <router-link
+                :to="{
+                  name: 'visualizar-vehiculo',
+                  params: { id: vehiculo.id },
+                }"
+              >
                 <button class="btn btn-primary me-3">
                   <i class="bi bi-eye"></i>
                   Ver
                 </button>
               </router-link>
-              <router-link :to="{ name: 'actualizar-vehiculo', params: { id: vehiculo.id } }">
+              <router-link
+                :to="{
+                  name: 'actualizar-vehiculo',
+                  params: { id: vehiculo.id },
+                }"
+              >
                 <button class="btn btn-warning me-3">
                   <i class="bi bi-arrow-clockwise"></i>
                   Actualizar
@@ -53,41 +75,48 @@
 </template>
 
 <script>
-import Mensaje from "@/components/Mensaje.vue"
-import { buscarVehiculosPorMarca, borrarVehiculoPorId } from "@/js/vehiculo.js"
+import Mensaje from "@/components/Mensaje.vue";
+import { buscarVehiculosPorMarca, borrarVehiculoPorId } from "@/js/vehiculo.js";
 export default {
   data() {
     return {
       listaDeVehiculos: [],
-      marca: '',
+      marca: "",
       msg: null,
-    }
+    };
   },
   components: {
-    Mensaje 
+    Mensaje,
   },
   methods: {
     async buscarVehiculo() {
       try {
-        this.listaDeVehiculos = await buscarVehiculosPorMarca(this.marca)
-      } catch(err) {
-        this.msg = {
-          tipoAlerta: 'info',
-          mensaje: 'Vehículo no encontrado',
-          adicional: `No se encontré ningún vehículo de la marca ${this.marca}`,
+        this.listaDeVehiculos = await buscarVehiculosPorMarca(this.marca);
+        if (this.listaDeVehiculos.length == 0) {
+          this.msg = {
+            tipoAlerta: "info",
+            mensaje: "Vehículo no encontrado",
+            adicional: `No se encontré ningún vehículo de la marca ${this.marca}`,
+          };
         }
+      } catch (err) {
+        this.msg = {
+          tipoAlerta: "info",
+          mensaje: "Vehículo no encontrado",
+          adicional: `No se encontré ningún vehículo de la marca ${this.marca}`,
+        };
       }
     },
     async borrar(id) {
-      borrarVehiculoPorId(id)
+      borrarVehiculoPorId(id);
       this.msg = {
-        tipoAlerta: 'danger',
-        mensaje: 'Vehículo eliminado',
-        adicional: 'Actualice la pagina por favor',
-      }
-    }
-  }
-}
+        tipoAlerta: "danger",
+        mensaje: "Vehículo eliminado",
+        adicional: "Actualice la pagina por favor",
+      };
+    },
+  },
+};
 </script>
 
 <style></style>
--- src/pages/empleado/RetirarunVehiculoReservado.vue
+++ src/pages/empleado/RetirarunVehiculoReservado.vue
@@ -2,14 +2,32 @@
   <div class="container mt-5">
     <h1>Retirar vehículo reservado</h1>
     <div class="form-floating mb-3">
-      <input type="text" name="reserva" id="reserva" class="form-control" v-model="numeroReserva">
+      <input
+        type="text"
+        name="reserva"
+        id="reserva"
+        class="form-control"
+        v-model="numeroReserva"
+      />
       <label for="reserva">Número de reserva:</label>
     </div>
     <button class="btn btn-primary" @click="buscarVehiculo">
       <i class="bi bi-search"></i>
       Buscar
     </button>
-    <Mensaje class="my-3" v-if="msg" :tipoAlerta="msg.tipoAlerta" :mensaje="msg.mensaje" :mensajeAdicional="msg.adicional"/>
+    
+    <Mensaje
+      class="my-3"
+      v-if="msg"
+      :tipoAlerta="msg.tipoAlerta"
+      :mensaje="msg.mensaje"
+      :mensajeAdicional="msg.adicional"
+    />
+
+    <div v-if="mensaje1" class="alert alert-info" role="alert">
+      {{ mensaje1 }}
+    </div>
+
     <div v-if="vehiculo.numero != ''">
       <table class="table mt-5">
         <thead class="table-dark">
@@ -27,10 +45,19 @@
             <th>{{ vehiculo.placa }}</th>
             <td>{{ vehiculo.modelo }}</td>
             <td>{{ vehiculo.estado }}</td>
-            <td>{{ `${vehiculo.fechaInicio.substring(0, 10)} - ${vehiculo.fechaFinal.substring(0, 10)}` }}</td>
+            <td>
+              {{
+                `${vehiculo.fechaInicio.substring(
+                  0,
+                  10
+                )} - ${vehiculo.fechaFinal.substring(0, 10)}`
+              }}
+            </td>
             <td>{{ vehiculo.cedula }}</td>
             <td>
-              <button class="btn btn-primary" @click="registrarRetiro">Retirar</button>
+              <button class="btn btn-primary" @click="registrarRetiro">
+                Retirar
+              </button>
             </td>
           </tr>
         </tbody>
@@ -40,46 +67,59 @@
 </template>
   
 <script>
-import { buscarVehiculoPorNumeroDeReserva, actualizarEstadoVehiculo } from "@/js/vehiculo.js"
+import {
+  buscarVehiculoPorNumeroDeReserva,
+  actualizarEstadoVehiculo,
+} from "@/js/vehiculo.js";
 export default {
   data() {
     return {
       numeroReserva: null,
       vehiculo: {
-        numero: '',
-        placa: '',
-        modelo: '',
-        estado: '',
-        fechaInicio: '',
-        fechaFinal: '',
-        cedula: '',
+        numero: "",
+        placa: "",
+        modelo: "",
+        estado: "",
+        fechaInicio: "",
+        fechaFinal: "",
+        cedula: "",
       },
       msg: null,
-    }
+      mensaje1:null,
+    };
   },
+
   methods: {
     buscarVehiculo() {
       try {
-        buscarVehiculoPorNumeroDeReserva(this.numeroReserva).then(r => this.vehiculo = r)
-      } catch(err) {
+        buscarVehiculoPorNumeroDeReserva(this.numeroReserva).then(
+          (r) => (this.vehiculo = r)
+        );
+      } catch (err) {
         this.msg = {
-          tipoAlerta: 'info',
-          mensaje: 'Vehículo no encontrado',
+          tipoAlerta: "info",
+          mensaje: "Vehículo no encontrado",
           adicional: `No se encontré ningún vehículo con el numero de reserva ${this.numeroReserva}`,
-        }
+        };
       }
     },
     registrarRetiro() {
-      actualizarEstadoVehiculo(this.numeroReserva)
-      this.vehiculo.estado = 'ND'
+      actualizarEstadoVehiculo(this.numeroReserva);
+      this.vehiculo.estado = "ND";
+      this.mensaje1="Se actualizo el estado de del vehículo",
+
       this.msg = {
-        tipoAlerta: 'info',
-        mensaje: 'Se actualizo el estado de del vehículo',
-        adicional: 'Se actualizo el estado de del vehículo ',
-      }
-    }
-  }
-}
+        tipoAlerta: "info",
+        mensaje: "Se actualizo el estado de del vehículo",
+        adicional: "Se actualizo el estado de del vehículo ",
+      };
+    },
+  },
+};
 </script>
 
-<style></style>
+<style>
+div{
+  margin-top: 10px;
+}
+</style>
